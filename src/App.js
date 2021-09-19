16e5e54dbb3aaf9628097413e3792a54716e9d55
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-19 16:21:07 +0600

    JSX and dynamic component and style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const singer ={
+    name:'mahfuz rahman',
+    job:'singer'
+  }
+
+  const singerStyle={
+    backgroundColor:'red',
+    color:'white'
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <div className="container">
+          <h3>Hi React</h3>
+          <p style={singerStyle}>Singer name: {singer.name} {singer.job}</p>
+          <p style={
+            {
+              backgroundColor:'#194d33',
+              color:'white',
+              fontSize:'24px'
+            }
+          }>Singer name: {singer.name} {singer.job}</p>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
